c4103f14c0eea88895e72764744294b36b77d0b0
Blue <bkioshn@gmail.com>
2021-05-09 13:41:08 +0700

    combine Pup UI and March API, implement getListFiles API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import './App.css'
+import {AxGetListFiles} from './api.js'
 
+const but1 = 'Student'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { register, handleSubmit } = useForm()
+	const [isStudent, setIsStudent] = useState('Student')
+
+ 
+
+
+
+
+	const toggleRole = () => {
+		if (isStudent == 'Teacher') {
+			setIsStudent('Student')
+		} else {
+			setIsStudent('Teacher')
+		}
+	}
+
+  // Render component by roles
+	const renderTeacherStudent = () => {
+		if (isStudent == 'Teacher') {
+			return (
+				<div className="column4">
+					<h3>See student assignments</h3>
+					<div className="GetStudentAssign">
+						<input {...register('SubjectID_GetStudent')} placeholder="Enter SubjectID" />
+						<br />
+						<input {...register('Topic_GetStudent')} placeholder="Enter topic name" />
+						<br />
+						<input type="submit" />
+					</div>
+				</div>
+			)
+		} else {
+			return (
+				<div className="column3">
+					<h3>See lectures & assignments</h3>
+					<div className="GetAssign">
+						<input {...register('SubjectID_GetAssign')} placeholder="Enter SubjectID" />
+						<br />
+						<input {...register('MemID_GetAssign')} placeholder="Enter Your ID" />
+						<br />
+						<input type="submit" />
+					</div>
+				</div>
+			)
+		}
+	}
+
+  // Handle upload
+  const onSubmitUpload = (data) => {
+    console.log('onclickUpload')
+    if (data.uploadTopic !== '' && data.uploadSubjectID !== '' && data.uploadMemID !== '' && data.uploadFile !== '') {
+      console.log(data)
+      // calling API
+    } 
+    
+  }
+
+  // Hadnle get all files
+  const onSubmitGetFile = async (data) => {
+    console.log('onClickGetFiles')
+    if (data.getFileSubjectID !== '' && data.getFileMemID !== '') {
+      // Callling API getListFiles
+      let getData = await AxGetListFiles(data.getFileSubjectID, data.getFileMemID)
+      console.log('get Data', getData)
+    }
+
+
+  }
+	return (
+		<div className="App">
+			<div className="role">
+				<button onClick={() => toggleRole()}>{isStudent}</button>
+			</div>
+			<div className="row">
+				<div className="column1">
+					<div className="Upload">
+						<form onSubmit={handleSubmit(onSubmitUpload)}>
+							<h3>Upload</h3>
+							<input {...register('uploadTopic')} placeholder="Enter topic name" />
+							<br />
+							<input {...register('uploadSubjectID')} placeholder="Enter SubjectID" />
+							<br />
+							<input {...register('uploadMemID')} placeholder="Enter Your ID" />
+							<br />
+							<input  {...register('uploadFile')} type="file" name="picture" />
+							<br />
+							<input type="submit" />
+						</form>
+					</div>
+				</div>
+				<div className="column2">
+					<h3>See your file</h3>
+					<div className="GetFile">
+          <form onSubmit={handleSubmit(onSubmitGetFile)}>
+
+						<input {...register('getFileSubjectID')} placeholder="Enter SubjectID" />
+						<br />
+						<input {...register('getFileMemID')} placeholder="Enter Your ID" />
+						<br />
+						<input type="submit" />
+          </form>
+					</div>
+
+				</div>
+			</div>
+      {renderTeacherStudent()}
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ src/api.js
@@ -0,0 +1,81 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+// const instance = axios.create({
+// 	baseURL: 'http://localhost:8084/getListFiles/?subjectID=CSS431&memberID=6122770215',
+// 	withCredentials: false,
+// 	headers: {
+// 		'Access-Control-Allow-Origin': '*',
+// 		'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+// 	},
+// })
+const ax = axios.create({
+	baseURL: 'http://localhost:8080/',
+	headers: {
+		'Content-type': 'application/json',
+	},
+})
+
+// Should be lowercase getListFiles
+const AxGetListFiles = (subjectID, memberID) => {
+	console.log(subjectID)
+	let params = {
+		subjectID: subjectID,
+		memberID: memberID,
+	}
+	return ax.get('/getListFiles/', {params})
+		.then((response) => {
+			console.log(response.data.args)
+			console.log('response: ', response)
+			console.log(JSON.stringify(response.data))
+			return(response.data)
+		})
+		.catch((err) => {
+			console.error(err)
+		})
+}
+const AxGetLectureAssignmentStudent = () => {
+	const [lectureAssignment, setLectureAssignment] = useState({})
+	axios
+		.get('http://localhost:8084/getLectureAssignmentStudent?subjectID=CSS431&isAssignment=1&memberID=6122770215')
+		.then((response) => {
+			console.log('response: ', response)
+			setLectureAssignment(JSON.stringify(response.data))
+			console.log(lectureAssignment)
+			return lectureAssignment
+		})
+		.catch((err) => {
+			console.error(err)
+		})
+}
+const AxGetAssignmentTeacher = () => {
+	const [assignmentTeacher, setAssignmentTeacher] = useState({})
+	axios
+		.get('http://localhost:8084/getAssignmentTeacher?subjectID=CSS431&topic=TestAs&isAssignment=1')
+		.then((response) => {
+			console.log('response: ', response)
+			setAssignmentTeacher(JSON.stringify(response.data))
+			console.log(assignmentTeacher)
+			return assignmentTeacher
+		})
+		.catch((err) => {
+			console.error(err)
+		})
+}
+
+const AxUpload = () => {
+	const [upload, setUpload] = useState({})
+	axios
+		.post('http://localhost:8084/upload', { topic: 'test31', fileName: 'Anessa' })
+		.then((response) => {
+			console.log('response:', response)
+			setUpload(JSON.stringify(response.data))
+			console.log(upload)
+			return upload
+		})
+		.catch((err) => {
+			console.error(err)
+		})
+}
+
+export { AxGetListFiles, AxGetLectureAssignmentStudent, AxGetAssignmentTeacher }
